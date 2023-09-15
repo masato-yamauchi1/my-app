a29e5371d407394f1054ee3f0b6a0a604aefbcd0
masato-yamauchi1 <oraora1800@yahoo.co.jp>
2023-09-15 23:59:48 +0900

    初期ページをControllerをとおして表示

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TopController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//コントローラーでトップを表示
+Route::get('/',[TopController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
