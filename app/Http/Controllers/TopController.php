9c191e76040ad9052123234922e66665778fae4e
masato-yamauchi1 <oraora1800@yahoo.co.jp>
2023-09-18 23:46:58 +0900

    TOPページ作成

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -17,7 +17,7 @@ class TopController extends Controller
         $data_list = DB::table('logics as a')
             ->leftJoin('indicators as b', 'a.main_indicator_id', '=', 'b.id')
             ->leftJoin('indicators as c', 'a.sub_indicator_id', '=', 'c.id')
-            ->select('a.*', 'b.indicator_name as main_indicator_name','c.indicator_name as sub_indicator_name')
+            ->select('a.*', 'b.indicator_name as main_indicator_name','c.indicator_name as sub_indicator_name','b.indicator_body as main_ing_body','c.indicator_body as sub_ing_body')
             ->where('a.active', '=', 1)
             ->where(function ( $query) {
                 $query->where('b.active', '=', 1)
@@ -30,37 +30,5 @@ class TopController extends Controller
             ->get();
         //dd(\DB::getQueryLog());
         return view('top' , ['data_list' => $data_list]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -11,9 +11,10 @@
 
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <header>
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
@@ -23,19 +24,44 @@
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
+                    </header>
                 </div>
             @endif
-
-            <h1>各自インジケーターのバックテストによる検証結果</h1>
-            @if ($data_list->isNotEmpty())
-            <ul>
-            @foreach ($data_list as $item)
-                <li>
-                    {{ $item->title }}
-                </li>
-            @endforeach
-            </ul>
+            <div class="py-12 ml-2 mr-2">
+                <p class="text-sm text-gray-700 dark:text-gray-500">
+                    <span class="text-red-600">このサイトはLrabel学習のために作成したテストサイトです。開発環境は以下のとおり。</span><br>
+                    Windows10 wls2 Ubuntu Docker(Docker Desktop) Laravel10(PHP 8.2) + breeze nginx MySql8.1 phpmyadmin<br>
+                    エディタはVisual Studio Code、ソース管理はgitを利用しています。
+                </p>
+                <h1 class="mt-8 text-gray-600 dark:text-gray-400 font-medium" style="font-size:24px;">各自インジケーターのバックテストによる検証結果</h1>
+                @if ($data_list->isNotEmpty())
+                    @foreach ($data_list as $item)
+                <main>
+                <h2 class="mt-8 text-gray-600 dark:text-gray-400 font-medium text-lg">{{ $item->title }}<h2>
+                <div>
+                    <img src="{{ Storage::url($item->graph_img_name) }}" width="90%" height="90%" class="border">
+                </div>
+                <p class="mt-2 text-sm text-gray-700 dark:text-gray-500">{!!  nl2br($item->logics_body) !!}</p>
+                <div class="mt-2">
+                    <img src="{{ Storage::url($item->result_img_name) }}" width="40%" height="40%" class="border">
+                </div>
+                <h3 class="mt-2 text-gray-600 dark:text-gray-400 font-medium text-lg">バックテストに用いた使用インジケーターについて</h3>
+                <dl>
+                    <dt class="mt-2 ml-2">メインインジケーター</dl>
+                    <dd class="ml-2">{{ $item->main_indicator_name }}</dd>
+                    <dd class="ml-2 mt-2 text-sm text-gray-700 dark:text-gray-500">{{$item->main_ing_body}}</dd>
+                </dl>
+                <dl>
+                    <dt class="mt-2 ml-2">サブインジケーター</dl>
+                    <dd class="ml-2">{{ $item->sub_indicator_name }}</dd>
+                    <dd class="ml-2 mt-2 text-sm text-gray-700 dark:text-gray-500">{{$item->sub_ing_body}}</dd>
+                </dl>
+                @endforeach
+                </main>
             @endif
+            
+            </div>
+            <footer class="mt2 ml2 text-sm text-gray-700 dark:text-gray-500 px-2">© 2023 yamauchi</footer>
         </div>
     </body>
 </html>
